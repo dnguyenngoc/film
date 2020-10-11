6ac373fadb33c52e882babef7d0ad044eb1964fb
Duy <duynguyenngoc@hotmail.com>
2020-10-11 11:13:46 +0700

    [initial] docker-compose development

--- backend/app/database/mysql.py
+++ backend/app/database/mysql.py
@@ -5,5 +5,5 @@ mydb = mysql.connector.connect(
   host=config.MYSQL_SERVER,
   user=config.MYSQL_USER,
   host=config.MYSQL_PASSWORD,
-  database=config.MYSQL_DB
+  database=config.MYSQL_DATABASE
 )
